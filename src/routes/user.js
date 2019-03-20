734e043552b2fdec94397c87628a4ca253b96ced
Julien Capellari <julien@capellari.net>
2019-03-20 11:08:23 +0100

    Suppression citoyen

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,23 +10,72 @@ export default function(db) {
     const router = Router();
 
     // mon profil
-    router.get('/', utils.user_guard(async function(req, res, next) { //route
-        const user = await Citoyen.getLoggedInUser(db, req);
-        console.log(user);
-        console.log(await user.getCompetances());
+    router.route('/')
+        .get(utils.user_guard(async function(req, res, next) { //route
+            const user = await Citoyen.getLoggedInUser(db, req);
+            console.log(user);
+            console.log(await user.getCompetances());
 
-        res.render('profil-benevole', { //lien entre la route et le pug profil
-            title: "Mon Profil",
+            res.render('profil-benevole', { //lien entre la route et le pug profil
+                title: "Mon Profil",
 
-            user: user,
-            documents:           await user.getDocuments(),
-            domaineintervention: await user.getDomainesIntervention(),
-            competances:         await user.getCompetances(),
-        });
-    }));
+                user: user,
+                documents:           await user.getDocuments(),
+                domaineintervention: await user.getDomainesIntervention(),
+                competances:         await user.getCompetances(),
+            });
+        }))
+        // Modification Informations citoyen
+        .post(utils.user_guard(async function(req, res, next) {
+            // Récups nouv infos
+            const { nom, prenom, adresse, tel, situation, permis } = req.body;
+
+            // Modif user
+            const user = await Citoyen.getLoggedInUser(db, req);
+
+            let needSave = false;
+
+
+            if (nom !== user.nom) {
+                user.nom = nom;
+                needSave = true;
+            }
+
+            if(prenom !== user.prenom){
+                user.prenom = prenom;
+                needSave = true;
+            }
+
+            if(adresse !== user.adresse){
+                user.adresse = adresse;
+                needSave = true;
+            }
+
+            if(tel !== user.tel){
+                user.tel = tel;
+                needSave = true;
+            }
+
+            if(situation !== user.situation){
+                user.situation = situation;
+                needSave = true;
+            }
+
+            if(permis !== user.permis){
+                user.permis = permis;
+                needSave = true;
+            }
+
+            if (needSave) {
+                user.save()
+                    .then(function() {
+                        res.redirect("/user");
+                    });
+            }
+        }));
 
     // mes candidatures
-    router.get('/candidatures', utils.login_guard(async function(req, res, next) { //route
+    router.get('/candidatures', utils.user_guard(async function(req, res, next) { //route
         const user = await Citoyen.getLoggedInUser(db, req);
         const postulations = await user.getPostulations();
         const missions = [];
@@ -47,63 +96,17 @@ export default function(db) {
         });
     }));
 
-
-    //modifications Informations citoyen
-    router.post('/',utils.login_guard(async function(req, res, next) {
-        // Récups nouv infos
-        const { nom, prenom, adresse, tel, situation, permis } = req.body;
-
-        // Modif user
-        const user = await Citoyen.getLoggedInUser(db, req);
-
-        let needSave = false;
-
-
-        if (nom !== user.nom) {
-            user.nom = nom;
-            needSave = true;
-        }
-
-        if(prenom !== user.prenom){
-            user.prenom = prenom;
-            needSave = true;
-        }
-
-        if(adresse !== user.adresse){
-            user.adresse = adresse;
-            needSave = true;
-        }
-
-        if(tel !== user.tel){
-            user.tel = tel;
-            needSave = true;
-        }
-
-        if(situation !== user.situation){
-            user.situation = situation;
-            needSave = true;
-        }
-
-        if(permis !== user.permis){
-            user.permis = permis;
-            needSave = true;
-        }
-
-        if (needSave) {
-            user.save()
-                .then(function() {
-                    res.redirect("/user");
-                });
-        }
-    }));
-
-
     //supprimer le profil citoyen
-    router.post('/supprCitoyen',utils.login_guard(async function(req, res, next) {
+    router.post('/supprCitoyen',utils.user_guard(async function(req, res, next) {
+        const user = await Citoyen.getLoggedInUser(db, req);
+        await user.delete();
 
+        Citoyen.disconnect(req);
+
+        res.redirect("/");
     }));
 
-
+    // Connexion
     router.post('/connexion', function(req, res, next) {
         // Déjà connecté ?
         if (req.session.connected) {
@@ -140,7 +143,6 @@ export default function(db) {
                 break;
         }
     });
-
     router.get('/deconnexion', utils.login_guard(async function(req, res, next) {
         // Déconnexion
         Association.disconnect(req);
